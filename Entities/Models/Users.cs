079777db3d17bb61f5ca816fb2ea58dc65462e9e
Serhat Tunçer <serhattuncert@gmail.com>
2025-02-02 10:29:31 +0300

    Token İşlemleri

--- Entities/Models/Users.cs
+++ Entities/Models/Users.cs
@@ -15,12 +15,14 @@ namespace Entities.Models
         public string? NameSurName { get; set; }
         public string? Email { get; set; }
         public bool EmailConfirmed { get; set; }
-        public string? PasswordHash {  get; set; }
+        public string? PasswordHash { get; set; }
         public string? PhoneNumber { get; set; }
         public bool PhoneNumberConfirmed { get; set; }
         public bool? TwoFactorEnabled {  get; set; }
         public DateTimeOffset? LockoutEnd {  get; set; }
         public bool LockoutEnabled { get; set; }
         public int AccessFailedCount { get; set; }
+
+        
     }
 }
